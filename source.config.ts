bcfef15253ac74ad2d7d4de62a828d0d651e217d
enoabasi <watermelonbit@gmail.com>
2024-10-21 00:50:21 +0100

    refac: set `dir` to `src` as root

--- source.config.ts
+++ source.config.ts
@@ -4,14 +4,16 @@ import {
 	frontmatterSchema,
 } from "fumadocs-mdx/config";
 
+import { rehypeCode } from "fumadocs-core/mdx-plugins";
 import { remarkInstall } from "fumadocs-docgen";
 import { z } from "zod";
 
-const blog = defineCollections({
-	dir: "content/blog",
+export const blog = defineCollections({
+	dir: "src/content/blog",
 	schema: frontmatterSchema.extend({
 		author: z.string().default("Anonymous").optional(),
 		date: z.string().optional(),
+		category: z.string().default("All").optional(),
 		published: z.boolean().default(true).optional(),
 	}),
 	type: "doc",
