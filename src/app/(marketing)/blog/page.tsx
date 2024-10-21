5083180e30e6ce195b08d6966d14857772603156
enoabasi <watermelonbit@gmail.com>
2024-10-21 11:51:10 +0100

    refac: only show `published` posts

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import Link from "next/link";
 import { Section } from "@/components/ui/section";
 import { cn } from "@/lib/cn";
@@ -25,24 +26,33 @@ export default function Page() {
 			<Section>
 				<h2>Recent blog posts</h2>
 				<div className="flex flex-col gap-2 mt-2">
-					{posts.map((post) => (
-						<Link
-							key={post.url}
-							href={post.url}
-							className={cn(
-								"flex flex-col bg-fd-card p-4 transition-colors hover:bg-fd-accent hover:text-fd-accent-foreground",
-								focusRing
-							)}
-						>
-							<h3>{post.data.title}</h3>
-							<p className="text-sm text-fd-muted-foreground [&:not(:first-child)]:mt-2">
-								{post.data.description}
-							</p>
-							<time className="pt-4 mt-auto text-xs text-fd-muted-foreground">
-								{new Date(post.data.date ?? post.file.name).toDateString()}
-							</time>
-						</Link>
-					))}
+					{posts.map((post) => {
+						return (
+							<Fragment key={post.url}>
+								{post.data.published ? (
+									<Link
+										href={post.url}
+										className={cn(
+											"flex flex-col bg-fd-card p-4 transition-colors hover:bg-fd-accent hover:text-fd-accent-foreground",
+											focusRing
+										)}
+									>
+										<hgroup>
+											<h3>{post.data.title}</h3>
+											<p className="text-sm text-fd-muted-foreground [&:not(:first-child)]:mt-2">
+												{post.data.description}
+											</p>
+										</hgroup>
+										<time className="pt-4 mt-auto text-xs text-fd-muted-foreground">
+											{new Date(
+												post.data.date ?? post.file.name
+											).toDateString()}
+										</time>
+									</Link>
+								) : null}
+							</Fragment>
+						);
+					})}
 				</div>
 			</Section>
 		</>
