fc30455157f462d0f85838776c571e0f593fe2b6
enoabasi <watermelonbit@gmail.com>
2024-10-21 16:35:59 +0100

    refac: use hard coded value

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
 	process.env.NODE_ENV === "development" || !process.env.VERCEL_URL
-		? new URL("http://localhost:3000")
-		: new URL(`https://${process.env.VERCEL_URL}`);
+		? "http://localhost:3000"
+		: `https://${process.env.VERCEL_URL}`;
